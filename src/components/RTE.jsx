b2840dfad16bb576185327977b05addc635f9cd1
SahilAxel <sahil.gidwani@axelerant.com>
2024-09-16 21:11:03 +0530

    Changed text edtior to quill

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,23 @@
-import { Editor } from "@tinymce/tinymce-react"
 import { Controller } from "react-hook-form"
+import ReactQuill from "react-quill"
+import "react-quill/dist/quill.snow.css"
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
+  const quillModules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", { color: [] }],
+      [
+        { align: null },
+        { align: "center" },
+        { align: "right" },
+        { align: "justify" },
+      ],
+      ["link", "image"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+    ],
+  }
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
@@ -9,42 +25,14 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         name={name || "content"}
         control={control}
         render={({ field: { onChange } }) => (
-          <Editor
-            apiKey={import.meta.env.VITE_TINYMCE_KEY}
-            initialValue={defaultValue}
-            init={{
-              initialValue: defaultValue,
-              height: 500,
-              menubar: false,
-              plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-              ],
-              toolbar:
-                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-              content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-            }}
-            onEditorChange={onChange}
-          />
+          <>
+            <ReactQuill
+              className="bg-white"
+              defaultValue={defaultValue}
+              theme="snow"
+              modules={quillModules}
+            />
+          </>
         )}
       />
     </div>
